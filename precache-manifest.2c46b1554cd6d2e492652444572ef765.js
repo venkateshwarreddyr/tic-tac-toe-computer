a92bf1e7e5940d6e0af8d1bccf697d123cab46df
unknown <venkat@pixelbrook.com>
2020-05-13 07:50:28 +0530

    Updates

---
+++ precache-manifest.2c46b1554cd6d2e492652444572ef765.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d789b896aff466834bccf2417bc39e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e8f6eaa2f6102569283",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5a25d5d6ba81dfe0f8d8",
+    "url": "/static/css/main.ee2b80a9.chunk.css"
+  },
+  {
+    "revision": "1e8f6eaa2f6102569283",
+    "url": "/static/js/2.6982e4bb.chunk.js"
+  },
+  {
+    "revision": "5a25d5d6ba81dfe0f8d8",
+    "url": "/static/js/main.d607520e.chunk.js"
+  },
+  {
+    "revision": "06836a7f10f9cad1e01c",
+    "url": "/static/js/runtime~main.7c675cf6.js"
+  }
+]);
