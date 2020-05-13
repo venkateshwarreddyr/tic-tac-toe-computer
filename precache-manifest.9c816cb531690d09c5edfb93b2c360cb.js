19fea99910cc9542704bab7010ff33f36fab4e30
unknown <venkat@pixelbrook.com>
2020-05-13 08:26:24 +0530

    Updates

--- precache-manifest.9c816cb531690d09c5edfb93b2c360cb.js
+++ precache-manifest.9c816cb531690d09c5edfb93b2c360cb.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "28a25683d03fd8fc134cab64d38cc086",
+    "revision": "43676f7cf10fc54a2becfbec4854dc6b",
     "url": "/tic-tac-toe-computer/index.html"
   },
   {
-    "revision": "1e8f6eaa2f6102569283",
+    "revision": "e1d21ff466c6b0c85f8f",
     "url": "/tic-tac-toe-computer/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "9e03909d03ddf18250d8",
+    "revision": "f6da2be9faf586da3dfd",
     "url": "/tic-tac-toe-computer/static/css/main.ee2b80a9.chunk.css"
   },
   {
-    "revision": "1e8f6eaa2f6102569283",
-    "url": "/tic-tac-toe-computer/static/js/2.6982e4bb.chunk.js"
+    "revision": "e1d21ff466c6b0c85f8f",
+    "url": "/tic-tac-toe-computer/static/js/2.20dc6e0d.chunk.js"
   },
   {
-    "revision": "9e03909d03ddf18250d8",
-    "url": "/tic-tac-toe-computer/static/js/main.9a0f1218.chunk.js"
+    "revision": "f6da2be9faf586da3dfd",
+    "url": "/tic-tac-toe-computer/static/js/main.aff63b1b.chunk.js"
   },
   {
     "revision": "c8321255ecb8089edb93",
